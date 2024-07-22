7678a7e0591f53815bb9d7c8e88a53a93e517bb2
Gersh0 <german.rivera@eia.edu.co>
2024-07-22 18:22:41 -0500

    chore: Update app module with Mongoose integration, serve static files, and add pets module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeedModule } from './seed/seed.module';
 import { MovieModule } from './movie/movie.module';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [SeedModule, MovieModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),//Permite servir archivos estáticos
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    SeedModule, 
+    MovieModule, 
+    PetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
